f3fa04b80b75b6055533116c7ff744e09dc0a17d
holifieldw <holifield04@gmail.com>
2025-10-19 23:12:42 +0800

    feat: app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PageLayout from "./layout/PageLayout";
+import { lazy } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const AnimeSearchPage = lazy(() => import("./pages/SearchAnimePage"));
+const AnimeDetailPage = lazy(() => import("./pages/AnimeDetailPage"));
 
+const App = () => {
   return (
-    <>
-      <div className='bg-red-400'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <BrowserRouter>
+      <PageLayout>
+        <Routes>
+          <Route path="/" element={<AnimeSearchPage />} />
+          <Route path="/details" element={<AnimeDetailPage />} />\
+          <Route
+            path="*"
+            element={
+              <div className="w-full h-full flex items-center justify-center">
+                <h1 className="text-2xl font-bold">Page not found</h1>
+              </div>
+            }
+          />
+        </Routes>
+      </PageLayout>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
