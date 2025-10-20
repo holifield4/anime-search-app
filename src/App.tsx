da1c00423922ec1e078edea22478fd7ff2aa40df
holifieldw <holifield04@gmail.com>
2025-10-20 23:07:16 +0800

    feat: add page not found and retrieve saved theme mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,39 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PageLayout from "./layout/PageLayout";
-import { lazy } from "react";
+import { lazy, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { changeTheme } from "./store/theme/theme.slice";
+import type { RootState } from "./store/store";
+import PageNotFound from "./components/loading/PageNotFound";
 
 const AnimeSearchPage = lazy(() => import("./pages/SearchAnimePage"));
 const AnimeDetailPage = lazy(() => import("./pages/AnimeDetailPage"));
 
 const App = () => {
+  const theme = useSelector((state: RootState) => state.theme);
+  const themeDispatch = useDispatch();
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("themeMode") || "light";
+    themeDispatch(changeTheme(savedTheme === "dark"));
+  }, [themeDispatch]);
+
   return (
     <BrowserRouter>
-      <PageLayout>
-        <Routes>
-          <Route path="/" element={<AnimeSearchPage />} />
-          <Route path="/details" element={<AnimeDetailPage />} />\
-          <Route
-            path="*"
-            element={
-              <div className="w-full h-full flex items-center justify-center">
-                <h1 className="text-2xl font-bold">Page not found</h1>
-              </div>
-            }
-          />
-        </Routes>
-      </PageLayout>
+      <div className={`${theme.darkMode ? "dark" : "ligt"}`}>
+        <PageLayout>
+          <Routes>
+            <Route path="/" element={<AnimeSearchPage />} />
+            <Route path="/anime/details/:id" element={<AnimeDetailPage />} />\
+            <Route
+              path="*"
+              element={
+                <PageNotFound/>
+              }
+            />
+          </Routes>
+        </PageLayout>
+      </div>
     </BrowserRouter>
   );
 };
