5d7bf0f94d681a87cce13864ae1aa92cbbb3200c
holifieldw <holifield04@gmail.com>
2025-10-20 23:06:23 +0800

    style: modify styling

--- src/components/textfield/SearchField.tsx
+++ src/components/textfield/SearchField.tsx
@@ -3,11 +3,11 @@ import { MagnifyingGlass } from "../../lib/icons/MagnifyingGlass";
 const SearchField = (props: React.InputHTMLAttributes<HTMLInputElement> ) => {
   return (
     <div className="relative text-ios-label-secondary">
-      <div className="w-56 focus-within:w-64 transition-all duration-300">
+      <div className="w-64 focus-within:w-80 transition-all duration-300">
         <input
           {...props}
           placeholder="Search"
-          className="input shadow-md focus:border-1 bg-ios-primary border-ios-secondary pl-5 pr-10 py-3 rounded-xl w-full outline-none h-12"
+          className="input shadow-sm dark:shadow-dark-label-2 focus:border-2 bg-ios-primary border-ios-secondary pl-5 pr-10 py-3 rounded-xl w-full outline-none h-12"
           name="search"
           aria-label="Search"
         />
